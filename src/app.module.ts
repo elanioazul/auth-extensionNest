4b55aa377dde26167afb055095c775fc466f5a85
hugo <hugo.herrador@gmail.com>
2023-02-16 13:05:07 +0100

    Set TypeOrm credentials to connect to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffesModule } from './coffes/coffes.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CoffesModule, UsersModule],
+  imports: [
+    CoffesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5435,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
