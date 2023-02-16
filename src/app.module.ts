b1793a5a167185d39da2699615665fb5cb04dc19
hugo <hugo.herrador@gmail.com>
2023-02-16 16:38:41 +0100

    Store env variables to work with jwt and make use of ConfigModule to access them

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AppService } from './app.service';
 import { CoffesModule } from './coffes/coffes.module';
 import { UsersModule } from './users/users.module';
 import { IamModule } from './iam/iam.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({}),
     CoffesModule,
     UsersModule,
     TypeOrmModule.forRoot({
