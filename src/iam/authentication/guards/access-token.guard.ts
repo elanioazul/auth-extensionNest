f054b0fa38d4b79e1c15c6ef9abb83f937addcda
hugo <hugo.herrador@gmail.com>
2023-02-18 17:07:47 +0100

    Inject jwtService to decode incoming JWT tokens
    Also we need JWT config to pass the sectret key down to the decode method

--- src/iam/authentication/guards/access-token.guard.ts
+++ src/iam/authentication/guards/access-token.guard.ts
@@ -1,11 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
-
+import { CanActivate, ExecutionContext, Inject, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { JwtService } from "@nestjs/jwt/dist";
+import jwtConfig from "src/iam/config/jwt.config";
+import { ConfigType } from "@nestjs/config";
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
+	constructor(
+		private readonly jwtService: JwtService,
+		@Inject(jwtConfig.KEY)
+		private readonly jwtConfiguration: ConfigType<typeof jwtConfig>
+	) {}
+	canActivate(
+		context: ExecutionContext
+	): boolean | Promise<boolean> | Observable<boolean> {
+		return true;
+	}
 }
