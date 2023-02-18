60711bd8786b21e99362a319ed7de17fd0e9aa20
hugo <hugo.herrador@gmail.com>
2023-02-18 18:15:11 +0100

    Add two static props

--- src/iam/authentication/guards/authentication.guard.ts
+++ src/iam/authentication/guards/authentication.guard.ts
@@ -1,14 +1,25 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
+import { AuthType } from "../enums/auth-type.enum";
 import { AccessTokenGuard } from "./access-token.guard";
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+	private static readonly defaultAuthType = AuthType.Bearer;
+	private readonly authTypeGuardMap: Record<
+		AuthType,
+		CanActivate | CanActivate[]
+	> = {
+		[AuthType.Bearer]: this.accessTokenGuard,
+		[AuthType.None]: { canActivate: () => true }
+	};
+
 	constructor(
 		private readonly reflector: Reflector,
 		private readonly accessTokenGuard: AccessTokenGuard
 	) {}
+
 	canActivate(
 		context: ExecutionContext
 	): boolean | Promise<boolean> | Observable<boolean> {
