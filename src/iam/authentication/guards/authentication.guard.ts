cc9e3340edfa8339f89cf198748ca5d78244eb79
hugo <hugo.herrador@gmail.com>
2023-02-18 18:07:57 +0100

    Create authentication guard, inlecting accessToken guard and reflector

---
+++ src/iam/authentication/guards/authentication.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { AccessTokenGuard } from "./access-token.guard";
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+	constructor(
+		private readonly reflector: Reflector,
+		private readonly accessTokenGuard: AccessTokenGuard
+	) {}
+	canActivate(
+		context: ExecutionContext
+	): boolean | Promise<boolean> | Observable<boolean> {
+		return true;
+	}
+}
