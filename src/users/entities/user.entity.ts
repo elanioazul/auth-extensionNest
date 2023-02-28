7121bf2b5dc3cf9a05ddffba83126cde60567446
hugo <hugo.herrador@gmail.com>
2023-02-28 18:46:29 +0100

    Create role enum to simulate roles
    But ideadlly they should be a resource in nest (table in db)

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "../enums/role.enum";
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+	@PrimaryGeneratedColumn()
+	id: number;
 
-  @Column({ unique: true })
-  email: string;
+	@Column({ unique: true })
+	email: string;
 
-  @Column()
-  password: string;
+	@Column()
+	password: string;
+
+	@Column({ enum: Role, default: Role.Regular })
+	role: Role;
 }
